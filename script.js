b0588c99614334b105baa2e1aa9680200855cc83
Nyiema Bayfield <ntmbayfield@gmail.com>
2018-04-27 04:08:38 -0700

    Upload solution for checkerboard exercise utilizing only Javascript to manipulate the DOM.

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-// Your JS goes here
+
+
+function makeTile(color) {
+  var tile = document.createElement("div");
+  tile.style.width = "11.1%";
+  tile.style.paddingBottom = "11.1%";
+  tile.style.float = "left";
+  tile.style.background = color;
+  return tile;
+}
+
+function createCheckerBoard(numberOfTiles) {
+  var tile=null;
+
+  for(var i=0;i<numberOfTiles;i++){
+      if (i%2===0){
+        color = "black";
+        tile = makeTile(color)
+        document.body.appendChild(tile);
+      } else {
+        if(j%2===0){
+        color = "red";
+        tile = makeTile(color)
+        document.body.appendChild(tile);
+      }  //closes else statement
+  }      //closes for loop
+}        //closes createCheckerBoard function
+
+createCheckerBoard(81);
