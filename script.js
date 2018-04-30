b7f1d1af57232f3802836e751fdb038ee6568d88
Nyiema Bayfield <ntmbayfield@gmail.com>
2018-04-29 18:23:51 -0700

    Upload changes to script.js file to make the checkerboard flash.

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-function randomBoard (grid) {
-  for (var i = 0; i < grid*grid; i++) {
-      var check = document.createElement('div');
-      check.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
-      check.style.width = 100/grid +'%';
-      check.style.paddingBottom = 100/grid + '%';
-      check.style.float = 'left';
-      document.body.appendChild(check);
-    }
-  }
 
-randomBoard(9);
+function randomBoard() {
+  for (var i=0; i<81; i++) {
+   var color = Math.floor(Math.random()*16777215).toString(16);
+   var check = document.createElement('div');
+             check.style.backgroundColor = '#'+ color;
+             check.style.width = '11%';
+             check.style.paddingBottom = '11%';
+             check.style.float = 'left';
+             document.body.appendChild(check);
+   }
+
+}
+
+randomBoard();
+setInterval(removeBoard, 2000);
+
+function removeBoard() {
+  var myNodelist = document.querySelectorAll('div');
+  var j;
+  for (j = 0; j < myNodelist.length; j++) {
+   document.body.removeChild(myNodelist[j]);
+  }
+  randomBoard();
+}
