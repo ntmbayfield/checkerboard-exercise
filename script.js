6f7242e1aeb8f9aa918fe24f006c3d9da03a78e2
Nyiema Bayfield <ntmbayfield@gmail.com>
2018-04-29 21:10:45 -0700

    Upload script.js file with changes to create checkerboard with two gradients.

--- script.js
+++ script.js
@@ -1,53 +1,27 @@
 
+ function makeTile(color) {
+   var tile = document.createElement("div");
+   tile.style.width = "11.1%";
+   tile.style.paddingBottom = "11.1%";
+   tile.style.float = "left";
+   tile.style.background = color;
+   return tile;
+ }
 
-function makeTile(color) {
-  var tile = document.createElement("div");
-  tile.style.width = "11.1%";
-  tile.style.paddingBottom = "11.1%";
-  tile.style.float = "left";
-  tile.style.background = color;
-  return tile;
-}
+ function createCheckerBoard(numberOfTiles) {
+   var tile=null;
 
-function createCheckerBoard(numberOfTiles) {
-  var tile=null;
+   for(var i=0;i<numberOfTiles;i++){
+       if (i%2===0){
+         color = `rgb(0, ${255-(i*2)}, ${i*2})`;
+         tile = makeTile(color)
+         document.body.appendChild(tile);
+       } else {
+           color = `rgb(${i*2}, 0, 0)`;
+           tile = makeTile(color)
+           document.body.appendChild(tile);
+       }  //closes else statement
+   }      //closes for loop
+ }        //closes createCheckerBoard function
 
-  for(var i=0;i<numberOfTiles;i++){
-      if (i%2===0){
-        color = "black";
-        tile = makeTile(color)
-        document.body.appendChild(tile);
-      } else {
-          color = "red";
-          tile = makeTile(color)
-          document.body.appendChild(tile);
-      }  //closes else statement
-  }      //closes for loop
-}        //closes createCheckerBoard function
-
-createCheckerBoard(81);
-
-//TO CREATE A CHECKERBOARD OF n x n SQUARES
-// function addElement (n)  {
-//
-//   for (var i = 0; i < n; i++) {
-//     for (var j = 0; j < n; j++) {
-//       if ((i+j)%2 === 0) {
-//         var check = document.createElement('div');
-//         document.body.appendChild(check);
-//         check.style.width = 100/n + '%';
-//         check.style.backgroundColor = '#000000';
-//         check.style.paddingBottom = 100/n + '%';
-//         check.style.float = 'left';
-//       } else {
-//         var check = document.createElement('div');
-//         document.body.appendChild(check);
-//         check.style.width = 100/n + '%';
-//         check.style.backgroundColor = 'red';
-//         check.style.paddingBottom = 100/n + '%';
-//         check.style.float = 'left';
-//         }
-//       }
-//     }
-//   }
-// addElement(9)
+ createCheckerBoard(81);
